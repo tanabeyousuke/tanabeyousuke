07cdfb027a4ff6603db0580fa11e266bf8599b37
tanabeyousuke <104883937+tanabeyousuke@users.noreply.github.com>
2023-09-17 11:12:26 +0900

    Add files via upload

---
+++ godot/godot/script/enemy/enemy.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var tama = preload('res://seen/tama.tscn')
+
+func cshot(x, y, angle, speed):
+	var bullet = tama.instantiate()
+	bullet.position.x = x
+	bullet.position.y = y
+	bullet.angle = angle
+	bullet.speed = speed
+	$"../".add_child(bullet)
+
+func cziki(x, y, angle, speed):
+	cshot(x, y, rad_to_deg(atan2(deg_to_rad(Global.zpy - position.y) , deg_to_rad(Global.zpx - position.x)) + angle), speed)
+
+
