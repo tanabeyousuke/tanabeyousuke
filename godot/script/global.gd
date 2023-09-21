15541f0fa64f4e2892646135e2c9fa614916a75a
tanabeyousuke <104883937+tanabeyousuke@users.noreply.github.com>
2023-09-21 14:23:41 +0900

    Add files via upload

---
+++ godot/script/global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var zpx
+var zpy
+var bullet = preload("res://seen/shot.tscn")
---
+++ godot/script/root.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+var ziki = preload("res://seen/ziki.tscn").instantiate()
+
+var i = 0
+var ha = 0
+
+func _ready():
+	ziki.position.x = 240
+	ziki.position.y = 450
+	add_child(ziki)
