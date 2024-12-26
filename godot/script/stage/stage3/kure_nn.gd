e09590f04f99fcea745a54218294886a2a4c4050
tanabeyousuke <youheiw256@gmail.com>
2024-12-26 09:02:55 +0900

    backup

--- godot/script/stage/stage3/kure_nn.gd
+++ godot/script/stage/stage3/kure_nn.gd
@@ -3,17 +3,6 @@ extends Sprite2D
 var count = 0
 var state = 0
 
-var tama = preload('res://seen/tama.tscn')
-
-func cshot(x, y, angle, speed):
-	var root = get_tree().root
-	var bullet = tama.instantiate()
-	bullet.position.x = x
-	bullet.position.y = y
-	bullet.angle = angle
-	bullet.speed = speed
-	root.add_child(bullet)
-
 func _process(delta):
 	match state:
 		0:
@@ -28,9 +17,6 @@ func _process(delta):
 				state = 2
 		2:
 			$"ufo".position.y = $"ufo".position.y - 5
-			$"prize".position.y = $"prize".position.y - 5
-			cshot($"prize".global_position.x, $"prize".global_position.y, count * 12.3456, 7)
-			cshot($"prize".global_position.x, $"prize".global_position.y, count * 12.3456 + 180, 7)
 
 			if $"ufo".position.y == 300:
 				state = 3
@@ -38,10 +24,6 @@ func _process(delta):
 			$"ufo/right".rotation = $"ufo/right".rotation - deg_to_rad(1)
 			$"ufo/left".rotation = $"ufo/left".rotation + deg_to_rad(1)
 
-			if $"prize".position.y != 1000:
-				cshot($"prize".global_position.x, $"prize".global_position.y, count * 12.3456, 7)
-				cshot($"prize".global_position.x, $"prize".global_position.y, count * 12.3456 + 180, 7)
-				$"prize".position.y = $"prize".position.y + 10
 				
 			if $"ufo/right".rotation < deg_to_rad(0):
 				state = 0
--- godot/script/stage/stage3/prize.gd
+++ godot/script/stage/stage3/prize.gd
@@ -1,12 +1,38 @@
 extends Area2D
 
 var hp = 3000
+var count = 0
 const type = "enemy"
+
+
+var tama = preload('res://seen/tama.tscn')
+
+func cshot(x, y, angle, speed):
+	var root = get_tree().root
+	var bullet = tama.instantiate()
+	bullet.position.x = x
+	bullet.position.y = y
+	bullet.angle = angle
+	bullet.speed = speed
+	root.add_child(bullet)
 
 func hit(damage):
 	hp = hp - damage
 
-
 func _process(delta):
+	match $"../".state:
+		2:
+			position.y = position.y - 5
+			cshot(global_position.x, global_position.y, count * 12.3456, 7)
+			cshot(global_position.x, global_position.y, count * 12.3456 + 180, 7)
+
+		3:
+			if position.y != 1000:
+				cshot(global_position.x, global_position.y, count * 12.3456, 7)
+				cshot(global_position.x, global_position.y, count * 12.3456 + 180, 7)
+				position.y = position.y + 10
+				
 	if hp < 0:
 		queue_free()
+
+	count = count + 1
